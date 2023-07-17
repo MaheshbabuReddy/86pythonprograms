c57dfd16dc5c2c63e8dbeedb6f78cc8ee36ce542
MaheshbabuReddy <maheshsvm123@gmail.com>
2023-07-17 18:57:33 +0530

    large number program

---
+++ bignumb.py
@@ -0,0 +1,12 @@
+list = []
+x=int(input('Enter number'))
+for ele in range(x+1):
+ val =int(input())
+ print('enter the', ele , val) 
+ list.append(val)
+ print(list)
+ print(max(list))
+   
+   
+   
+   
