aa71ff9847e44e1422d8bab4d1f9fbf28e6cb1a5
MaheshbabuReddy <maheshsvm123@gmail.com>
2023-07-18 19:38:46 +0530

    Neg_Pos program

---
+++ Neg_Pos.py
@@ -0,0 +1,7 @@
+x = int(input('Enter the number'))
+if x >= 0:
+    print('This number is a positive number')
+elif x <= 0:
+    print('This number is a Negative Number')
+else:
+    print('This number invalid ')
