8a95a421272a6128d3697e0367a2d40b0c2da726
MaheshbabuReddy <maheshsvm123@gmail.com>
2023-07-19 15:11:40 +0530

    Added some Changes

--- Factorial.py
+++ Factorial.py
@@ -1,6 +1,11 @@
 n=int(input('Enter the number'))
 nfact= 1
-for i in range(1,n+1):
-     nfact=nfact*i
+if n<0 or n==0:
+     print('Their is no Factorials for 0')
+elif n==1:
+     print('The Factorial for 1 is 1')
+else:
+    for i in range(1,n+1):
+      nfact=nfact*i
     
-     print('The Factorial of ' ,n,'is',nfact)
+      print('The Factorial of ' ,n,'is',nfact)
